2eb3b8dd2c5ef6725f774a42693abc5dacd3650a
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2021-12-12 18:44:11 +0530

    Optimized add location screen

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _previewImageUrl == null
+              ? const Text(
+                  'No location chosen!',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl.toString(),
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton.icon(
+              icon: const Icon(
+                Icons.location_on,
+              ),
+              label: const Text('Current location'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: () {},
+            ),
+            FlatButton.icon(
+              icon: const Icon(
+                Icons.map,
+              ),
+              label: const Text('Select on map'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
