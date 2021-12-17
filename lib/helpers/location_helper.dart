d8e72489fc3df5b186d4545a8d6d3783b3bfd34d
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2021-12-17 08:33:07 +0530

    Added dynamic map input

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,11 +1,14 @@
-const MAPBOX_API_KEY =
-    'pk.eyJ1Ijoic291bXlhZGVlcDY4NDUiLCJhIjoiY2t4NDY4a202MGI0dTJ4bzVxMmJiamFwMCJ9.T4Mson612ExC6qh3O1oNiQ';
+import 'package:flutter_config/flutter_config.dart';
+
+// final String? apiKey = FlutterConfig.get('MAPBOX_API_KEY');
+const MAPBOX_API_KEY = 'pk.eyJ1Ijoic291bXlhZGVlcDY4NDUiLCJhIjoiY2t4NGdkNWNxMXYyazJwb2JnNm40c2U4OCJ9.tBQ2K1Uqkl6LON0Aj3D6vQ';
 
 class LocationHelper {
   static String generateLocationPreviewImage({
     double? latitude,
     double? longitude,
   }) {
+    // return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-l($longitude,$latitude)/$longitude,$latitude,14.25,0,0/600x300?access_token=$apiKey';
     return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-l($longitude,$latitude)/$longitude,$latitude,14.25,0,0/600x300?access_token=$MAPBOX_API_KEY';
     
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+// import 'package:flutter_config/flutter_config.dart';
 
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
 import './screens/add_place_screen.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await FlutterConfig.loadEnvVariables();
+  
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
